37a3f9cf3ab500915496b191146b5887ffa88f10
manojk353 <killerkrock353@gmail.com>
2020-11-16 13:00:08 +0530

    initial commit

---
+++ Alrethon-Services/src/main/java/com/Albenero/Alrethon/AlrethonApplication.java
@@ -0,0 +1,13 @@
+package com.Albenero.Alrethon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlrethonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlrethonApplication.class, args);
+	}
+
+}
---
+++ Alrethon-Services/src/test/java/com/Albenero/Alrethon/AlrethonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Albenero.Alrethon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlrethonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
